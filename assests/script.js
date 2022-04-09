a1ef056dca0adb6e283fa3a767669090701c1be4
Sophia C <95388964+Sophtron5000@user.noreply.github.com>
2022-04-09 15:12:44 -0700

    add current date with MomentJS

--- assests/script.js
+++ assests/script.js
@@ -1,6 +1,23 @@
 // GIVEN a weather dashboard with form inputs
 // WHEN I search for a city
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
+var now = moment();
+// $("today").text.dayjs().format(M/D/YY)
+$("#today").text(now.format("MMM Do, YYYY"))
+console.log(moment())
+
+//function that takes in a string(city)
+function citySearch(str) {
+        //fetch city to geocache coordinates
+
+    //fetch weather condition with geocache
+
+        //save the search to local storage
+        //append history to appear under search bar
+}
+//append the information to the page in unique classes
+
+
 // WHEN I view current weather conditions for that city
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
 // WHEN I view the UV index
@@ -9,3 +26,5 @@
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
+
+
